b6fcaa4e376c46b6d97a242c79a584b06eef08d6
Italo Gabriel <italo.pereira@pagtel.com.br>
2023-11-24 16:35:22 -0300

    create adapters

--- application/product_test.go
+++ application/product_test.go
@@ -1,7 +1,8 @@
 package application_test
 
 import (
-	"github.com/italog/go-hexagonal/application"
+	"github.com/italog/go-hexagonal"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -32,4 +33,27 @@ func TestProduct_Disable(t *testing.T)  {
 	product.Price = 10
 	err = product.Disable()
 	require.Equal(t, "The price must be zero in order to have the product disabled", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal zero", err.Error())
 }
